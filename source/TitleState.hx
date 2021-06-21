dc7f78e2bb2ec2c8fa022a76527ca5752abc75e5
Lucky56 <55949451+Lucky-56@users.noreply.github.com>
2021-06-21 02:44:05 +0200

    polymod removed + fix dumb mistake

--- source/TitleState.hx
+++ source/TitleState.hx
@@ -302,7 +302,7 @@ class TitleState extends MusicBeatState
 			new FlxTimer().start(2, function(tmr:FlxTimer)
 			{
 				FlxG.switchState(new MainMenuState());
-			}
+			});
 		}
 
 		if (pressedEnter && !skippedIntro && initialized)
